7e3aef041dd9dba78c895693571adb0875c69a95
Nate Bunderson <nate.bunderson@asirobots.com>
2021-07-11 01:34:37 -0600

    Local install and testing

--- package.sh
+++ package.sh
@@ -15,5 +15,7 @@ function clean {
 clean
 python3 setup.py sdist bdist_wheel
 python3 setup.py test
+pip3 install -e .  # for local testing and use
 python3 -m twine upload dist/*
+
 # use the username __token__ and the password (in the notes of pypi in vault)
